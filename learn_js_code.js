1216cdd437dd2bcdb31a4a8dd4d14dbcd0364342
nabin shrestha <nabinsth@gmail.com>
2024-05-09 22:26:13 +0545

    add a js file in codebase

---
+++ learn_js_code.js
@@ -0,0 +1,48 @@
+let myYoutubename = "hiteshdotcom";
+let anothername = myYoutubename;
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@google.com";
+
+let userThree = userTwo;
+userThree.email = "nabin@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+console.log(userThree.email);
+
+const gameName = new String("hiteshhc");
+for (let i = 0; i < gameName.length; i++) {
+  console.log(gameName[i]);
+}
+
+// String Methods
+//************* *//
+// length
+// endswith;
+// constructor;
+// concatenate;
+// charat(2);
+// indexOf('t')
+// substring(0,4)
+// trim
+
+// replace
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne = "    hitest   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
